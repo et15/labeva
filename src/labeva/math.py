d9bfa384eb742722d871612031f764fa268c031d
Erik Thiel <erik.thiel@students.uni-mainz.de>
2023-07-14 13:38:07 +0900

    docs(project): add docstings to everything

--- src/labeva/math.py
+++ src/labeva/math.py
@@ -1,3 +1,7 @@
+"""
+Tools for processing math data. Either numerical result, some kind of numpy object or rendered string.
+"""
+
 import numpy as np
 
 
@@ -5,16 +9,18 @@ def magnitude(x: float) -> int:
     """
     calculate magnitude of given value
 
-    :param x: value
+    Args:
+        x (float): value
 
-    :return: magnitude
+    Returns:
+        Orders of magnitude in decimal system
     """
     return np.choose(
         x == 0,  # bool values, used as indices to the array
         [
             np.int_(np.log10(np.abs(x)) // 1),  # if false
             0,  # if true
-        ]
+        ],
     )
 
 
@@ -22,11 +28,13 @@ def chisquare(y_exp, y_obs, y_obs_error) -> float:
     """
     calculate chi square value: (y_expected - y_observed)^2 / delta_y_observed^2
 
-    :param y_exp: expected y-values
-    :param y_obs: observed y-values
-    :param y_obs_error: uncertainties of observed y-values
+    Args:
+        y_exp: expected y-values
+        y_obs: observed y-values
+        y_obs_error: uncertainties of observed y-values
 
-    :return: chi square value
+    Returns:
+        chi square value
     """
     return sum(
         [
@@ -40,38 +48,55 @@ def average(series) -> (float, float):
     """
     calculate average and standard error of average
 
-    :param series: Series of values
-    :return: average, standard error of average
+    Args:
+        series: Series of values
+
+    Returns:
+        average, standard error of average
     """
     return np.average(series), np.std(series, ddof=1) / np.sqrt(len(series))
 
 
-def gaussian_fwhm(d, d_d):
-    return 2 * np.sqrt(2 * np.log(2)) * d, 2 * np.sqrt(2 * np.log(2)) * d_d
+def gaussian_fwhm(std: float, d_std: float) -> (float, float):
+    """
+    Calculate full width at half maximum of a gaussian.
+
+    Args:
+        std: standard derivation \\(\\sigma\\)
+        d_std: error of standard derivation
+
+    Returns:
+        FWHM of gaussian
+    """
+    return 2 * np.sqrt(2 * np.log(2)) * std, 2 * np.sqrt(2 * np.log(2)) * d_std
 
 
 def ls_minmax(data, num: int = 1000) -> np.ndarray:
     """
     returns linear spaced samples in the interval [min(data), max(data)] with length num=1000
 
-    :param data: series of data to gain min and max values
-    :param num: number of samples
+    Args:
+        data: series of data to gain min and max values
+        num: number of samples
 
-    :return: linear spaced samples
+    Returns:
+        linear spaced samples
     """
     return np.linspace(np.min(data), np.max(data), num)
 
 
-def error_str(value: float, error: float, frmt="plain", unit=None) -> str:
+def error_str(value: float, error: float, frmt: str = "plain", unit: str | None = None) -> str:
     """
     render value with uncertainty in string with right amount of decimal numbers in magnitude of value
 
-    :param value: value
-    :param error: uncertainty of value
-    :param frmt: format `plain`, `tex` or `si`
-    :param unit: print unit behind value
+    Args:
+        value: value
+        error: uncertainty of value
+        frmt: format `plain`, `tex` or `si`
+        unit: print unit behind value
 
-    :return: (value +- error)(e+-mag)
+    Returns:
+        (value +- error)(e+-mag)
     """
     # todo implement siunitx format
     if magnitude(error) > magnitude(value):
